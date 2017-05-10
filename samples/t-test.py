15b15c086f5115eb2bd491994bc28553aaa1325c
Thomas Graichen <thomas.graichen@sap.com>
2017-05-10 12:38:37 +0200

    export everything we can get via prometheus

--- samples/t-test.py
+++ samples/t-test.py
@@ -5,7 +5,6 @@ from prometheus_client import start_http_server, Summary, Counter, Gauge
 from tools import cli
 from pyVim.connect import SmartConnect, Disconnect
 import atexit
-#import getpass
 import ssl
 import datetime
 
@@ -13,11 +12,8 @@ def main():
 
     args = cli.get_args()
 
-    # Start up the server to expose the metrics.
+    # Start up the server to expose the prometheus metrics
     start_http_server(8000)
-
-    # define a gauge
-    g = Gauge('vmwaresamplegauge', 'vmware sample gauge', ['name'])
 
     # Connect to the host without SSL signing
     try:
@@ -40,23 +36,6 @@ def main():
 
 
 ##############################################################################
-    # create a mapping from performance stats to their counterIDs
-    # counterInfo: [performance stat => counterId]
-    # performance stat example: cpu.usagemhz.LATEST
-    # counterId example: 6
-    print('Start: Date now: %s' % datetime.datetime.now())
-    counterInfo = {}
-    counterids=perfManager.QueryPerfCounterByLevel(level=1)
-
-    for c in counterids:
-        fullName = c.groupInfo.key + "." + c.nameInfo.key + "." + c.rollupType
-        counterInfo[fullName] = c.key
-
-    print('QueryPerfCounterByLevel: Date now: %s' % datetime.datetime.now())
-    collected=[ 'cpu.usage.average', 'cpu.usagemhz.average', 'cpu.ready.summation', 'mem.usage.average', 'mem.swapinRate.average', 'mem.swapoutRate.average', 'mem.vmmemctl.average', 'mem.consumed.average', 'mem.overhead.average', 'disk.usage.average'  ]
-    counterIDs = [counterInfo[k] for k in collected if k in counterInfo]
-
-##############################################################################
     # create a list of vim.VirtualMachine objects so that we can query them for statistics
     container = content.rootFolder
     viewType = [vim.VirtualMachine]
@@ -72,14 +51,40 @@ def main():
     print("Count of VMs:" + str(count_vms))
     print('CreateContainerView: Start: Date now: %s' % datetime.datetime.now())
 
+##############################################################################
+    # create a mapping from performance stats to their counterIDs
+    # counterInfo: [performance stat => counterId]
+    # performance stat example: cpu.usagemhz.LATEST
+    # counterId example: 6
+    print('Start: Date now: %s' % datetime.datetime.now())
+    counterInfo = {}
+    counterids=perfManager.QueryPerfCounterByLevel(level=4)
+
+    g = {}
+    for c in counterids:
+        fullName = c.groupInfo.key + "." + c.nameInfo.key + "." + c.rollupType
+        print(fullName + ": " + str(c.key))
+        counterInfo[fullName] = c.key
+
+        # define a gauges for the counter ids
+        g[fullName.replace('.','_')] = Gauge(fullName.replace('.','_'), fullName.replace('.','_'), ['vmware_name'])
+
+    print('QueryPerfCounterByLevel: Date now: %s' % datetime.datetime.now())
+    collected=[ 'cpu.usage.average', 'cpu.usagemhz.average', 'cpu.ready.summation', 'mem.usage.average', 'mem.swapinRate.average', 'mem.swapoutRate.average', 'mem.vmmemctl.average', 'mem.consumed.average', 'mem.overhead.average', 'disk.usage.average'  ]
+#    counterIDs = [counterInfo[k] for k in collected if k in counterInfo]
+    counterIDs = [m.key for m in counterids]
+
 # REQUEST MetricId
 #    metricIDs = [vim.PerformanceManager.MetricId(counterId=c,
 #                                                   instance="*")
 #                  for c in counterIDs]
 #    print('MetricId: Start: Date now: %s' % datetime.datetime.now())
 ########################################################################################
+    # loop over all vmware machines
     for child in children:
+     # only consider machines which have an annotation and are powered on
      if child.summary.config.annotation and child.summary.runtime.powerState=="poweredOn":
+        # split the multi-line annotation into a dict per property (name, project-id, ...)
         lis=child.summary.config.annotation.split('\n')
         d = dict(s.rsplit(':',1) for s in filter(None, lis))
 
@@ -90,32 +95,35 @@ def main():
         print('MetricId: Start: Date now: %s' % datetime.datetime.now())
 
 #        print(metricIDs)
-# REQUEST QuerySpec
+# REQUEST QuerySpec - build query spec for the next metric query
         spec = vim.PerformanceManager.QuerySpec(maxSample=1,
                                                 entity=child,
                                                 metricId=metricIDs,
                                                 intervalId=20)
 
         print('QuerySpec: Start: Date now: %s' % datetime.datetime.now())
-# REQUEST QueryStats
+# REQUEST QueryStats - get metrics from vcenter
         result = perfManager.QueryStats(querySpec=[spec])
         print('QueryStats: Start: Date now: %s' % datetime.datetime.now())
         # Loop through the results and print the output
         output = ""
         for r in result:
-            if child.summary.config.annotation and child.summary.runtime.powerState=="poweredOn":
-               for val in result[0].value:
-                    if val:
-                        output += "id:" + child.summary.config.name + ","
-                        output += counterInfo.keys()[
-                                  counterInfo.values().index(val.id.counterId)]
-                        output += ": " + str(val.value[0]) + ", name:" + d['name'] + ", project_id:" + d['projectid'] + "\n"
+#            if child.summary.config.annotation and child.summary.runtime.powerState=="poweredOn":
+            # loop over the metrics
+            for val in result[0].value:
+                 if val:
+                     # print vmware name
+#                     output += "id:" + child.summary.config.name + ","
+                     # print key names
+#                     output += counterInfo.keys()[counterInfo.values().index(val.id.counterId)]
+                     # print key values plus metadata
+#                     output += ": " + str(val.value[0]) + ", name:" + d['name'] + ", project_id:" + d['projectid'] + "\n"
 #               output += "\n"
-
-        print(filter(None, output))
-
-        if counterInfo.keys()[counterInfo.values().index(val.id.counterId)] == 'cpu.usage.average':
-            g.labels(d['name']).set(val.value[0])
+                     # try to remove empty lines (maybe does not seem to work)
+#                     print(filter(None, output))
+#        if counterInfo.keys()[counterInfo.values().index(val.id.counterId)] == 'cpu.usage.average':
+                # send metrics to prometheus exporter
+                     g[counterInfo.keys()[counterInfo.values().index(val.id.counterId)].replace('.','_')].labels(d['name']).set(val.value[0])
 
 if __name__ == "__main__":
     main()
