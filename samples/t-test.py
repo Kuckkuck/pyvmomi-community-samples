6e65bcba60b1f507138552f4c21800f61fdfdf09
Thomas Graichen <thomas.graichen@sap.com>
2017-05-10 17:44:17 +0200

    add config parsing from olaf and other changes

--- samples/t-test.py
+++ samples/t-test.py
@@ -7,21 +7,38 @@ from pyVim.connect import SmartConnect, Disconnect
 import atexit
 import ssl
 import datetime
+from yamlconfig import YamlConfig
+import argparse
+
+defaults = {
+            'vcenter_ip': 'localhost',
+            'vcenter_user': 'administrator@vsphere.local',
+            'vcenter_password': 'password',
+            'ignore_ssl': True
+            }
 
 def main():
 
-    args = cli.get_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", help="Specify config file", metavar="FILE")
+    args, remaining_argv = parser.parse_known_args()
+    config = YamlConfig(args.config, defaults)
 
     # Start up the server to expose the prometheus metrics
     start_http_server(8000)
 
     # Connect to the host without SSL signing
+    si=None
+    context = None
+    if config['main']['ignore_ssl'] and \
+       hasattr(ssl, "_create_unverified_context"):
+       context = ssl._create_unverified_context()
+
     try:
-        context = ssl._create_unverified_context()
-        si = SmartConnect(host=args.host,
-             user=args.user,
-             pwd=args.password,
-             port=int(args.port),
+        si = SmartConnect(host=config['main']['host'],
+        user=config['main']['user'],
+             pwd=config['main']['password'],
+             port=int(config['main']['port']),
              sslContext=context)
         atexit.register(Disconnect, si)
 
@@ -49,9 +66,9 @@ def main():
     # counterInfo: [performance stat => counterId]
     # performance stat example: cpu.usagemhz.LATEST
     # counterId example: 6
-    print('Start perfManager.QueryPerfCounterByLevel: %s' % datetime.datetime.now())
+#    print('Start perfManager.QueryPerfCounterByLevel: %s' % datetime.datetime.now())
     counterids=perfManager.QueryPerfCounterByLevel(level=4)
-    print('Done perfManager.QueryPerfCounterByLevel: %s' % datetime.datetime.now())
+#    print('Done perfManager.QueryPerfCounterByLevel: %s' % datetime.datetime.now())
 
     for c in counterids:
         fullName = c.groupInfo.key + "." + c.nameInfo.key + "." + c.rollupType
@@ -63,21 +80,25 @@ def main():
 
 #        print('QueryPerfCounterByLevel: Date now: %s' % datetime.datetime.now())
 #        collected=[ 'cpu.usage.average', 'cpu.usagemhz.average', 'cpu.ready.summation', 'mem.usage.average', 'mem.swapinRate.average', 'mem.swapoutRate.average', 'mem.vmmemctl.average', 'mem.consumed.average', 'mem.overhead.average', 'disk.usage.average'  ]
-    #    counterIDs = [counterInfo[k] for k in collected if k in counterInfo]
-    counterIDs = [m.key for m in counterids]
+
+    collected=config.get('main').get('vm_metrics')
+    if collected:
+        counterIDs = [counterInfo[k] for k in collected if k in counterInfo]
+    else:
+        counterIDs = [m.key for m in counterids]
 
     while True:
 
 # REQUEST CreateContainerView
-        print('Start content.viewManager.CreateContainerView: %s' % datetime.datetime.now())
+#        print('Start content.viewManager.CreateContainerView: %s' % datetime.datetime.now())
         containerView = content.viewManager.CreateContainerView(container,
                                                             viewType,
                                                             recursive)
-        print('Done content.viewManager.CreateContainerView: %s' % datetime.datetime.now())
+#        print('Done content.viewManager.CreateContainerView: %s' % datetime.datetime.now())
 
         children = containerView.view
         count_vms=len(children)
-        print("Number of VMs:" + str(count_vms))
+        print("===> Number of VMs:" + str(count_vms))
 
 
     # REQUEST MetricId
@@ -97,25 +118,25 @@ def main():
                     d = dict(s.rsplit(':',1) for s in filter(None, lis))
 
             # REQUEST MetricID
-                    print('Start vim.PerformanceManager.MetricId: %s' % datetime.datetime.now())
+#                    print('Start vim.PerformanceManager.MetricId: %s' % datetime.datetime.now())
                     metricIDs = [vim.PerformanceManager.MetricId(counterId=c,
                                                                  instance="*")
                                  for c in counterIDs]
-                    print('Done vim.PerformanceManager.MetricId: %s' % datetime.datetime.now())
+#                    print('Done vim.PerformanceManager.MetricId: %s' % datetime.datetime.now())
 
             #        print(metricIDs)
             # REQUEST QuerySpec - build query spec for the next metric query
-                    print('Start vim.PerformanceManager.QuerySpec: %s' % datetime.datetime.now())
+#                    print('Start vim.PerformanceManager.QuerySpec: %s' % datetime.datetime.now())
                     spec = vim.PerformanceManager.QuerySpec(maxSample=1,
                                                             entity=child,
                                                             metricId=metricIDs,
                                                             intervalId=20)
-                    print('Done vim.PerformanceManager.QuerySpec: %s' % datetime.datetime.now())
+#                    print('Done vim.PerformanceManager.QuerySpec: %s' % datetime.datetime.now())
 
             # REQUEST QueryStats - get metrics from vcenter
-                    print('Start perfManager.QueryStats: %s' % datetime.datetime.now())
+#                    print('Start perfManager.QueryStats: %s' % datetime.datetime.now())
                     result = perfManager.QueryStats(querySpec=[spec])
-                    print('Done perfManager.QueryStats: %s' % datetime.datetime.now())
+#                    print('Done perfManager.QueryStats: %s' % datetime.datetime.now())
                     # Loop through the results and print the output
                     output = ""
                     for r in result:
@@ -136,7 +157,7 @@ def main():
                             # send metrics to prometheus exporter
 
             # remove cloud.sap from hostname for a shorter vcenter_name label via replace
-                                 g['vcenter_' + counterInfo.keys()[counterInfo.values().index(val.id.counterId)].replace('.','_')].labels(d['name'], d['projectid'], args.host.replace('.cloud.sap','')).set(val.value[0])
+                                 g['vcenter_' + counterInfo.keys()[counterInfo.values().index(val.id.counterId)].replace('.','_')].labels(d['name'], d['projectid'], config['main']['host'].replace('.cloud.sap','')).set(val.value[0])
             except vmodl.fault.ManagedObjectNotFound:
                     print('===> lost machine')
 
