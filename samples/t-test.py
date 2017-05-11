d31849430376cdeb9e198e15cca714264f4ec9f8
Thomas Graichen <thomas.graichen@sap.com>
2017-05-11 09:50:32 +0200

    cleanup, doc, formatting

--- samples/t-test.py
+++ samples/t-test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+# python interface to vmware performance metrics
 from pyVmomi import vim, vmodl
-from prometheus_client import start_http_server, Summary, Counter, Gauge
+# prometheus export functionality
+from prometheus_client import start_http_server, Gauge
 from tools import cli
 from pyVim.connect import SmartConnect, Disconnect
 import atexit
@@ -10,36 +12,39 @@ import datetime
 from yamlconfig import YamlConfig
 import argparse
 
+# vcenter connection defaults
 defaults = {
-            'vcenter_ip': 'localhost',
-            'vcenter_user': 'administrator@vsphere.local',
-            'vcenter_password': 'password',
-            'ignore_ssl': True
-            }
+    'vcenter_ip': 'localhost',
+    'vcenter_user': 'administrator@vsphere.local',
+    'vcenter_password': 'password',
+    'ignore_ssl': True
+}
+
 
 def main():
 
+    # config file parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", help="Specify config file", metavar="FILE")
+    parser.add_argument(
+        "-c", "--config", help="Specify config file", metavar="FILE")
     args, remaining_argv = parser.parse_known_args()
     config = YamlConfig(args.config, defaults)
 
-    # Start up the server to expose the prometheus metrics
-    start_http_server(8000)
-
-    # Connect to the host without SSL signing
-    si=None
+    # check for insecure ssl option
+    si = None
     context = None
     if config['main']['ignore_ssl'] and \
        hasattr(ssl, "_create_unverified_context"):
-       context = ssl._create_unverified_context()
+        context = ssl._create_unverified_context()
 
+    # connect to vcenter
     try:
-        si = SmartConnect(host=config['main']['host'],
-        user=config['main']['user'],
-             pwd=config['main']['password'],
-             port=int(config['main']['port']),
-             sslContext=context)
+        si = SmartConnect(
+            host=config['main']['host'],
+            user=config['main']['user'],
+            pwd=config['main']['password'],
+            port=int(config['main']['port']),
+            sslContext=context)
         atexit.register(Disconnect, si)
 
     except IOError as e:
@@ -51,115 +56,105 @@ def main():
     content = si.RetrieveContent()
     perfManager = content.perfManager
 
-
-##############################################################################
     # create a list of vim.VirtualMachine objects so that we can query them for statistics
     container = content.rootFolder
     viewType = [vim.VirtualMachine]
     recursive = True
 
     counterInfo = {}
-    g = {}
+    gauge = {}
 
-    ##############################################################################
     # create a mapping from performance stats to their counterIDs
     # counterInfo: [performance stat => counterId]
     # performance stat example: cpu.usagemhz.LATEST
     # counterId example: 6
-#    print('Start perfManager.QueryPerfCounterByLevel: %s' % datetime.datetime.now())
-    counterids=perfManager.QueryPerfCounterByLevel(level=4)
-#    print('Done perfManager.QueryPerfCounterByLevel: %s' % datetime.datetime.now())
+    counterids = perfManager.QueryPerfCounterByLevel(level=4)
 
+    # start up the http server to expose the prometheus metrics
+    start_http_server(8000)
+
+    # loop over all counterids and build their full name and a dict relating it to the ids
     for c in counterids:
         fullName = c.groupInfo.key + "." + c.nameInfo.key + "." + c.rollupType
         print(fullName + ": " + str(c.key))
         counterInfo[fullName] = c.key
 
-        # define a gauges for the counter ids
-        g['vcenter_' + fullName.replace('.','_')] = Gauge('vcenter_' + fullName.replace('.','_'), 'vcenter_' + fullName.replace('.','_'), ['vmware_name', 'project_id', 'vcenter_name'])
+        # define a dict of gauges for the counter ids
+        gauge['vcenter_' + fullName.replace('.', '_')] = Gauge(
+            'vcenter_' + fullName.replace('.', '_'),
+            'vcenter_' + fullName.replace('.', '_'),
+            ['vmware_name', 'project_id', 'vcenter_name'])
 
-#        print('QueryPerfCounterByLevel: Date now: %s' % datetime.datetime.now())
-#        collected=[ 'cpu.usage.average', 'cpu.usagemhz.average', 'cpu.ready.summation', 'mem.usage.average', 'mem.swapinRate.average', 'mem.swapoutRate.average', 'mem.vmmemctl.average', 'mem.consumed.average', 'mem.overhead.average', 'disk.usage.average'  ]
-
-    collected=config.get('main').get('vm_metrics')
-    if collected:
-        counterIDs = [counterInfo[k] for k in collected if k in counterInfo]
+    # in case we have a set of metric to handle use those, otherwise use all we can get
+    selected_metrics = config.get('main').get('vm_metrics')
+    if selected_metrics:
+        counterIDs = [
+            counterInfo[i] for i in selected_metrics if i in counterInfo
+        ]
     else:
-        counterIDs = [m.key for m in counterids]
+        counterIDs = [i.key for i in counterids]
 
+    # infinite loop for getting the metrics
     while True:
 
-# REQUEST CreateContainerView
-#        print('Start content.viewManager.CreateContainerView: %s' % datetime.datetime.now())
-        containerView = content.viewManager.CreateContainerView(container,
-                                                            viewType,
-                                                            recursive)
-#        print('Done content.viewManager.CreateContainerView: %s' % datetime.datetime.now())
+        # create containerview to get a list of vmware machines
+        containerView = content.viewManager.CreateContainerView(
+            container, viewType, recursive)
 
         children = containerView.view
-        count_vms=len(children)
-        print("===> Number of VMs:" + str(count_vms))
+        count_vms = len(children)
+        print("INFO: number of vms - " + str(count_vms))
 
-
-    # REQUEST MetricId
-    #    metricIDs = [vim.PerformanceManager.MetricId(counterId=c,
-    #                                                   instance="*")
-    #                  for c in counterIDs]
-    #    print('MetricId: Start: Date now: %s' % datetime.datetime.now())
-    ########################################################################################
         # loop over all vmware machines
         for child in children:
             try:
-                 # only consider machines which have an annotation and are powered on
-                 if child.summary.config.annotation and child.summary.runtime.powerState=="poweredOn":
-                    print("vm: " + child.summary.config.name)
+                # only consider machines which have an annotation and are powered on
+                if child.summary.config.annotation and child.summary.runtime.powerState == "poweredOn":
+                    print("INFO: current vm processed - " +
+                          child.summary.config.name)
                     # split the multi-line annotation into a dict per property (name, project-id, ...)
-                    lis=child.summary.config.annotation.split('\n')
-                    d = dict(s.rsplit(':',1) for s in filter(None, lis))
+                    annotation_lines = child.summary.config.annotation.split(
+                        '\n')
+                    # the filter is for filtering out empty lines
+                    annotations = dict(
+                        s.rsplit(':', 1)
+                        for s in filter(None, annotation_lines))
 
-            # REQUEST MetricID
-#                    print('Start vim.PerformanceManager.MetricId: %s' % datetime.datetime.now())
-                    metricIDs = [vim.PerformanceManager.MetricId(counterId=c,
-                                                                 instance="*")
-                                 for c in counterIDs]
-#                    print('Done vim.PerformanceManager.MetricId: %s' % datetime.datetime.now())
+                    # get a list of metricids for this vm in preparation for the stats query
+                    metricIDs = [
+                        vim.PerformanceManager.MetricId(
+                            counterId=i, instance="*") for i in counterIDs
+                    ]
 
-            #        print(metricIDs)
-            # REQUEST QuerySpec - build query spec for the next metric query
-#                    print('Start vim.PerformanceManager.QuerySpec: %s' % datetime.datetime.now())
-                    spec = vim.PerformanceManager.QuerySpec(maxSample=1,
-                                                            entity=child,
-                                                            metricId=metricIDs,
-                                                            intervalId=20)
-#                    print('Done vim.PerformanceManager.QuerySpec: %s' % datetime.datetime.now())
+                    # query spec for the metric stats query
+                    # we might get the interval from PerfProviderSummary later ...
+                    spec = vim.PerformanceManager.QuerySpec(
+                        maxSample=1,
+                        entity=child,
+                        metricId=metricIDs,
+                        intervalId=20)
 
-            # REQUEST QueryStats - get metrics from vcenter
-#                    print('Start perfManager.QueryStats: %s' % datetime.datetime.now())
+                    # get metric stats from vcenter
                     result = perfManager.QueryStats(querySpec=[spec])
-#                    print('Done perfManager.QueryStats: %s' % datetime.datetime.now())
-                    # Loop through the results and print the output
-                    output = ""
-                    for r in result:
-            #            if child.summary.config.annotation and child.summary.runtime.powerState=="poweredOn":
-                        # loop over the metrics
-                        for val in result[0].value:
-                             if val:
-                                 # print vmware name
-            #                     output += "id:" + child.summary.config.name + ","
-                                 # print key names
-            #                     output += counterInfo.keys()[counterInfo.values().index(val.id.counterId)]
-                                 # print key values plus metadata
-            #                     output += ": " + str(val.value[0]) + ", name:" + d['name'] + ", project_id:" + d['projectid'] + "\n"
-            #               output += "\n"
-                                 # try to remove empty lines (maybe does not seem to work)
-            #                     print(filter(None, output))
-            #        if counterInfo.keys()[counterInfo.values().index(val.id.counterId)] == 'cpu.usage.average':
-                            # send metrics to prometheus exporter
 
-            # remove cloud.sap from hostname for a shorter vcenter_name label via replace
-                                 g['vcenter_' + counterInfo.keys()[counterInfo.values().index(val.id.counterId)].replace('.','_')].labels(d['name'], d['projectid'], config['main']['host'].replace('.cloud.sap','')).set(val.value[0])
+                    # loop over the metrics
+                    for val in result[0].value:
+                        if val:
+                            # send gauges to prometheus exporter: metricname and value with
+                            # labels: vm name, project id and vcenter name
+                            gauge['vcenter_' +
+                                  counterInfo.keys()[counterInfo.values(
+                                  ).index(val.id.counterId)].replace(
+                                      '.', '_')].labels(
+                                          annotations['name'],
+                                          annotations['projectid'],
+                                          config['main']['host'].replace(
+                                              '.cloud.sap',
+                                              '')).set(val.value[0])
+
             except vmodl.fault.ManagedObjectNotFound:
-                    print('===> lost machine')
+                print('INFO: a machine disappeared during processing')
+
 
 if __name__ == "__main__":
     main()
